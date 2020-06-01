e4564dd8572f6885763a2c8484c96f523d2ba52c
Dominik Blak <bldominikak@gmial.com>
2020-06-01 16:03:57 +0200

    secondVersion

--- homepage/js/script.js
+++ homepage/js/script.js
@@ -1,41 +1,22 @@
-console.log(
-  "%cWitaj Neo w Matriksie ! :-)",
-  "color: rgb(56, 168, 50); font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;"
-);
-const btnProjects = document.getElementById("projects");
-const btnBackground = document.getElementById("background");
-const btnContact = document.getElementById("contact");
+console.log("Witaj Neo w Matriksie !");
+const block = document.body;
+block.classList.add("mySite");
+const btnFont = document.querySelector(".font");
+const btnBackground = document.querySelector(".background");
+const btnPhoto = document.querySelector(".removePhoto");
+const about = document.querySelector(".aboutMe");
+const photo = document.querySelector(".photo");
 
-let show = document.getElementsByClassName("showProjects");
-let flag = false;
-let flagContact = false;
-const showProjects = () => {
-  show.innerHTML = "";
-  flag = !flag;
-  if (flag == true) {
-    const git = document.createElement("div");
-
-    git.innerHTML =
-      "<h1>https://github.com/dominikblak</h1> <p>Skopiować i wkleić link. Serwisy blokują przekierowania z sandboxów (?)</p><a href='https://github.com/dominikblak'><img src='img/github-153-675523.png' alt='Konto GitHub' /></a>";
-    show = document.body.appendChild(git);
-    flag = !flag;
-  }
+const changeColorFont = () => {
+  about.classList.toggle("colorFont");
 };
 const changeBackground = () => {
   document.body.classList.toggle("whiteTheame");
 };
 
-const showContact = () => {
-  show.innerHTML = "";
-  flagContact = !flagContact;
-  if (flagContact == true) {
-    const contact = document.createElement("div");
-    contact.innerHTML =
-      "<h1>https://www.linkedin.com/in/dominik-blak-060416176/</h1> <p>Skopiować i wkleić link. Serwisy blokują przekierowania z sandboxów (?)</p><a href='https://www.linkedin.com/in/dominik-blak-060416176/'><img src='img/official-linkedin-logo-tile-300x300.png' alt='Konto GitHub' /></a>";
-    show = document.body.appendChild(contact);
-    flagContact = !flagContact;
-  }
+const removePhoto = () => {
+  photo.remove();
 };
-btnProjects.addEventListener("click", showProjects);
+btnFont.addEventListener("click", changeColorFont);
 btnBackground.addEventListener("click", changeBackground);
-btnContact.addEventListener("click", showContact);
+btnPhoto.addEventListener("click", removePhoto);
