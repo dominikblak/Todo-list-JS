9c696618738c8503ad6e713b061d33c29e467db9
Dominik Blak <bldominikak@gmial.com>
2020-06-09 16:42:46 +0200

    if else

--- homepage/js/script.js
+++ homepage/js/script.js
@@ -6,8 +6,6 @@ const btnBackground = document.querySelector(".background");
 const btnPhoto = document.querySelector(".removePhoto");
 const about = document.querySelector(".aboutMe");
 const photo = document.querySelector(".photo");
-const img = new Image();
-img.src = "img/wifibooth_3264-IMG_2806kolor.jpg";
 
 const changeColorFont = () => {
   about.classList.toggle("colorFont");
@@ -17,12 +15,13 @@ const changeBackground = () => {
 };
 
 const addPhoto = () => {
-  photo.append(img);
-  btnPhoto.innerHTML = "Usuń zdjęcie";
-  btnPhoto.addEventListener("click", () => {
-    photo.removeChild(img);
+  if (photo.style.display === "none") {
+    photo.style.display = "block";
+    btnPhoto.innerHTML = "Ukryj zdjęcie";
+  } else {
+    photo.style.display = "none";
     btnPhoto.innerHTML = "Pokaż zdjęcie";
-  });
+  }
 };
 btnFont.addEventListener("click", changeColorFont);
 btnBackground.addEventListener("click", changeBackground);
