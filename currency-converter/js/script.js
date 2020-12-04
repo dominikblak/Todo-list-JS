c4895c3d28bb3a7cb2ec2299f63a29bf8ea7b962
Dominik Blak <bldominikak@gmial.com>
2020-12-04 20:07:30 +0100

    counter

--- currency-converter/js/script.js
+++ currency-converter/js/script.js
@@ -1,1 +1,21 @@
-let amount = document.querySelector("counter__files");
+let amount = document.querySelector(".counter__files");
+let formElement = document.querySelector(".counter__form");
+let choice = document.querySelector(".counter__files--margin");
+let currencyValue = document.querySelector(".counter__value");
+let result;
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let amountValue = amount.value;
+  let choiceValue = choice.value;
+
+  if (choiceValue === "euro") {
+    result = amountValue * 4.47;
+  } else if (choiceValue === "usd") {
+    result = amountValue * 3.68;
+  } else {
+    result = amountValue * 2.11;
+  }
+
+  currencyValue.innerText = `${result.toFixed(2)} zł`;
+});
