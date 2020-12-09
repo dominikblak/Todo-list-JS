b80a6d81684d99908f7c697ba237e05ec5582d6c
Dominik Blak <bldominikak@gmial.com>
2020-12-09 18:40:06 +0100

    refactoring

--- currency-converter/js/script.js
+++ currency-converter/js/script.js
@@ -1,13 +1,17 @@
-let amount = document.querySelector(".counter__files");
-let formElement = document.querySelector(".counter__form");
-let choice = document.querySelector(".counter__files--margin");
-let currencyValue = document.querySelector(".counter__value");
+let amountElement = document.querySelector(".js-files");
+let formElement = document.querySelector(".js-form");
+let choiceElement = document.querySelector(".js-counter");
+let currencyValElement = document.querySelector(".js-value");
 let result;
+
+let EuroRate = 4.47;
+let UsdRare = 3.68;
+let EscRate = 2.11;
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
-  let amountValue = amount.value;
-  let choiceValue = choice.value;
+  let amountValue = amountElement.value;
+  let choiceValue = choiceElement.value;
 
   if (choiceValue === "euro") {
     result = amountValue * 4.47;
@@ -17,5 +21,5 @@ formElement.addEventListener("submit", (e) => {
     result = amountValue * 2.11;
   }
 
-  currencyValue.innerText = `${result.toFixed(2)} zł`;
+  currencyValElement.innerText = `${result.toFixed(2)} zł`;
 });
