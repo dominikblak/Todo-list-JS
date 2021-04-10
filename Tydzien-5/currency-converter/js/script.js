a2690a52e39312357c655fb9eb6ffe692eed5bad
Dominik Blak <bldominikak@gmial.com>
2021-04-10 11:00:51 +0200

    global values

--- Tydzien-5/currency-converter/js/script.js
+++ Tydzien-5/currency-converter/js/script.js
@@ -1,29 +1,31 @@
-let amountElement = document.querySelector(".js-files");
-let formElement = document.querySelector(".js-form");
-let choiceElement = document.querySelector(".js-counter");
-let currencyValElement = document.querySelector(".js-value");
-let result;
+{
+  let amountElement = document.querySelector(".js-files");
+  let formElement = document.querySelector(".js-form");
+  let choiceElement = document.querySelector(".js-counter");
+  let currencyValElement = document.querySelector(".js-value");
+  let result;
 
-let EuroRate = 4.47;
-let UsdRate = 3.68;
-let UgxRate = 2.11;
+  let EuroRate = 4.47;
+  let UsdRate = 3.68;
+  let UgxRate = 2.11;
 
-formElement.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let amountValue = +amountElement.value;
-  let choiceValue = choiceElement.value;
+  formElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let amountValue = +amountElement.value;
+    let choiceValue = choiceElement.value;
 
-  switch (choiceValue) {
-    case "eur":
-      result = amountValue * EuroRate;
-      break;
-    case "usd":
-      result = amountValue * UsdRate;
-      break;
-    case "ugx":
-      result = amountValue * UgxRate;
-      break;
-  }
+    switch (choiceValue) {
+      case "eur":
+        result = amountValue * EuroRate;
+        break;
+      case "usd":
+        result = amountValue * UsdRate;
+        break;
+      case "ugx":
+        result = amountValue * UgxRate;
+        break;
+    }
 
-  currencyValElement.innerText = `${result.toFixed(2)} zł`;
-});
+    currencyValElement.innerText = `${result.toFixed(2)} zł`;
+  });
+}
--- Tydzien-5/homepage/js/script.js
+++ Tydzien-5/homepage/js/script.js
@@ -1,33 +1,33 @@
-function welcome() {
-  console.log("Witaj Neo w Matriksie ;-)");
-}
-welcome();
-const block = document.body;
-block.classList.add("mySite");
-const btnFont = document.querySelector(".introduction__buttons--font");
-const btnBackground = document.querySelector(
-  ".introduction__buttons--background"
-);
-const btnPhoto = document.querySelector(".introduction__buttons--removePhoto");
-const about = document.querySelector(".introduction");
-const photo = document.querySelector(".introduction__photo");
-
-const changeColorFont = () => {
-  document.body.classList.toggle("colorFont");
-};
-const changeBackground = () => {
-  document.body.classList.toggle("whiteTheame");
-};
-
-const addPhoto = () => {
-  if (photo.style.display === "none") {
-    photo.style.display = "block";
-    btnPhoto.innerHTML = "Ukryj zdjęcie";
-  } else {
-    photo.style.display = "none";
-    btnPhoto.innerHTML = "Pokaż zdjęcie";
+{
+  function welcome() {
+    console.log("Witaj Neo w Matriksie ;-)");
   }
-};
-btnFont.addEventListener("click", changeColorFont);
-btnBackground.addEventListener("click", changeBackground);
-btnPhoto.addEventListener("click", addPhoto);
+  welcome();
+  const block = document.body;
+  block.classList.add("mySite");
+  const btnFont = document.querySelector(".introduction__buttons--font");
+  const btnBackground = document.querySelector(".introduction__buttons--background");
+  const btnPhoto = document.querySelector(".introduction__buttons--removePhoto");
+  const about = document.querySelector(".introduction");
+  const photo = document.querySelector(".introduction__photo");
+
+  const changeColorFont = () => {
+    document.body.classList.toggle("colorFont");
+  };
+  const changeBackground = () => {
+    document.body.classList.toggle("whiteTheame");
+  };
+
+  const addPhoto = () => {
+    if (photo.style.display === "none") {
+      photo.style.display = "block";
+      btnPhoto.innerHTML = "Ukryj zdjęcie";
+    } else {
+      photo.style.display = "none";
+      btnPhoto.innerHTML = "Pokaż zdjęcie";
+    }
+  };
+  btnFont.addEventListener("click", changeColorFont);
+  btnBackground.addEventListener("click", changeBackground);
+  btnPhoto.addEventListener("click", addPhoto);
+}
