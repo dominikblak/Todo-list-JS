e357073675e70549f2428e12bb7b99f1f0d3c4b0
Dominik Blak <bldominikak@gmial.com>
2021-04-29 17:59:23 +0200

    refactoring4

--- Tydzien-4/homepage/js/script.js
+++ Tydzien-4/homepage/js/script.js
@@ -2,7 +2,7 @@ console.log("Witaj Neo w Matriksie !");
 const buttonFont = document.querySelector(".js-buttonFont");
 const buttonBackground = document.querySelector(".js-buttonBackground");
 const buttonPhoto = document.querySelector(".js-buttonRemovePhoto");
-const photo = document.querySelector(".introduction__photo");
+const photo = document.querySelector(".header__photo");
 const container = document.querySelector(".js-container");
 const body = document.querySelector(".js-body");
 
