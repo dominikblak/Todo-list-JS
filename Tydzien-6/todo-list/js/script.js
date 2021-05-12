acb003f965f1d2b2dd217c3e15a59decde811dde
Dominik Blak <bldominikak@gmial.com>
2021-05-12 19:11:58 +0200

    remove task

--- Tydzien-6/todo-list/js/script.js
+++ Tydzien-6/todo-list/js/script.js
@@ -14,10 +14,18 @@
     let htmlString = "";
     for (const task of tasks) {
       htmlString += `<li ${task.done ? ' style="text-decoration : line-through"' : ""}>
+      <button class="js-remove">Usuń</button>
       ${task.content}
       </li>`;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        tasks.splice(index, 1);
+        render();
+      });
+    });
   };
   const addNewTask = (newTaskContent) => {
     tasks.push({
