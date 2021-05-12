b4658fdca69c05bb8776b081306a9a4a10d7b018
Dominik Blak <bldominikak@gmial.com>
2021-05-12 18:45:46 +0200

    function addNewTask, EventListener, templateString

--- Tydzien-6/todo-list/js/script.js
+++ Tydzien-6/todo-list/js/script.js
@@ -9,15 +9,33 @@
       done: true,
     },
   ];
+
   const render = () => {
     let htmlString = "";
     for (const task of tasks) {
-      htmlString += `<li>${task.content}</li>`;
+      htmlString += `<li ${task.done ? ' style="text-decoration : line-through"' : ""}>
+      ${task.content}
+      </li>`;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
   const init = () => {
     render();
+    const form = document.querySelector(".js-form");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+      if (newTaskContent === "") {
+        return;
+      }
+      addNewTask(newTaskContent);
+    });
   };
   init();
 }
