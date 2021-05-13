449c929e970a59f883c4813e1a5cdb63a88d39e2
Dominik Blak <bldominikak@gmial.com>
2021-05-13 18:14:15 +0200

    toggle task done

--- Tydzien-6/todo-list/js/script.js
+++ Tydzien-6/todo-list/js/script.js
@@ -9,11 +9,25 @@
       done: true,
     },
   ];
-
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
   const render = () => {
     let htmlString = "";
     for (const task of tasks) {
       htmlString += `<li ${task.done ? ' style="text-decoration : line-through"' : ""}>
+      <button class="js-done">Zrobione ?</button>
       <button class="js-remove">Usuń</button>
       ${task.content}
       </li>`;
@@ -22,16 +36,15 @@
     const removeButtons = document.querySelectorAll(".js-remove");
     removeButtons.forEach((removeButton, index) => {
       removeButton.addEventListener("click", () => {
-        tasks.splice(index, 1);
-        render();
+        removeTask(index);
       });
     });
-  };
-  const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
+    const toogleDoneButtons = document.querySelectorAll(".js-done");
+    toogleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
     });
-    render();
   };
   const onFormSubmit = (event) => {
     event.preventDefault();
